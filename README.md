bc8921a6869460a63132e3edc0f69bd5e2cd81e8
Adam Gomez <adam.daniel.gomez.787@gmail.com>
2020-10-17 04:08:36 -0500

    Improved readme, started scratchpad

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-This is the repository that will contain my clustering project.
+# Predicting Log Error from Zillow's Value Estimate of So-Cal Homes
+
+This project is an extension of a previous project. View the github of the previous project here: [**Predicting Tax Assessed Home Values**](https://github.com/Estimating-Home-Value/regression-project)
+
+## Description
+> For this project you will continue working with the zillow dataset. Continue to use the 2017 properties and predictions data for single unit / single family homes.
+
+> In addition to continuing work on your previous project, you should incorporate clustering methodologies on this project.
+
+> Your audience for this project is a data science team. The presentation will consist of a notebook demo of the discoveries you made and work you have done related to uncovering what the drivers of the error in the zestimate is.
+
+## Goals
+1. Identify significant drivers of log error for single unit properties in 2017
+2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+3. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+4. Aenean viverra accumsan massa, vitae tincidunt risus laoreet sed. Donec fermentum, mauris quis porttitor mollis, ante magna hendrerit lorem, in blandit risus turpis id nisi.
+
+## Deliverables
+1. A clearly named final notebook. This notebook will be what you present and should contain plenty of markdown documentation and cleaned up code.
+2. A README that explains what the project is, how to reproduce you work, and your notes from project planning.
+3. A Python module or modules that automate the data acquisistion and preparation process. These modules should be imported and used in your final notebook.
+
+## Additional Project Requirements
+- Data Acquisition: Data is collected from the codeup cloud database with an appropriate SQL query
+- Data Prep: Column data types are appropriate for the data they contain
+- Data Prep: Missing values are investigated and handled
+- Data Prep: Outliers are investigated and handled
+- Exploration: the interaction between independent variables and the target variable is explored using visualization and statistical testing
+- Exploration: Clustering is used to explore the data. A conclusion, supported by statistical testing and visualization, is drawn on whether or not the clusters are helpful/useful. At least 3 combinations of features for clustering should be tried.
+- Modeling: At least 4 different models are created and their performance is compared. One model is the distinct combination of algorithm, hyperparameters, and features.
+- Best practices on data splitting are followed
+- The final notebook has a good title and the documentation within is sufficiently explanatory and of high quality
+- Decisions and judgment calls are made and explained/documented
+- All python code is of high quality
+
+## Data Dictionary
+| Column | Description |
+| --- | ---|
+| id | Autoincremented unique index id for each property |
+| bathroomcnt | Number of Bathrooms; Includes halfbaths as 0.5 |
+| bedroomcnt | Number of Bedrooms |
+| calculatedbathnbr | Precise meaning unknown, but appears to be redundant with bathroomcnt and bedroomcnt |
+| calculatedfinishedsquarefeet | Total square feet of home; doesn't include property square feet |
+| finishedsquarefeet12| Unknown, but appears to be redundant with calculatedfinishedsquarefeet | 
+| fips | Federal Information Processing System codes used to identify unique geographical areas | 
+| fullbathcnt | Number of full bathrooms |
+| latitude | The latitude of the property
+| longitude | The longitude of the property |
+| lotsizesquarefeet| The size of the total property lot |
+| propertycountylandusecode | Unknown, but represents categorical government code |
+| propertylandusetypeid |  Categorical variable describing the general type of property |
+| rawcensustractandblock | Government id for each property linked to geographic location |
+| regionidcity | Categorical variable identifying geographic location |
+| regionidcounty | Categorical variable identifying geographic location |
+| roomcnt | Number of rooms |
+| yearbuilt | The year the house was built |
+| structuretaxvaluedollarcnt | The tax assessed value of only the property structure in USD | 
+| assessmentyear | Year that the tax value was assessed |
+| landtaxvaluedollarcnt | The tax assessed value of only the land lot for the property |
+| taxamount | The amount paid in taxes by the landowner in USD |
+| taxvaluedollarcnt | The tax accessed value of the property in USD |
+| censustractandblock | Redundant with rawcensustractandblock |
+| logerror | Unknown |
+| transactiondate | Four digit year, two digit month, two digit date | 
+| taxrate | Rounded derived value by dividing the taxamount by the taxvaluedollarcnt and multiplying by 100 |
+| County | County the property is located in | 
+
+Additional columns were present in the zillow database but had greater than 20% null values and were dropped during initial consideration. 
+
+## Data Validation
+The following considerations were taken with the data:
+1. Initial SQL query produced 20,364 records that met the following requirements:
+    * Transaction Date between 2017-01-01 and 2017-12-31
+    * Property classified as one of the following Types:
+        * Single Family Residential
+        * Rural Residence
+        * Mobile Home
+        * Townhouse
+        * Condominium
+        * Row House
+        * Bungalow
+        * Manufactured, Modular, Prefabricated Homes
+        * Patio Home
+        * Inferred Single Family Residence
+2. Records containing 0 bathrooms, 0 bedrooms, or null square footage were dropped
+3. Duplicate records were dropped. These entries may represent "back-to-back" closings on the same day between three parties.
+
+## Managing Outliers
+Outliers were identified in the following features. Due to the significant influence of outliers on clustering techniques, the following considerations were made:
+- Feature 1: Consideration
+- Feature 2: Consideration
+- Feature 3: Consideration
+- Feature 4: Consideration
+
+## Key Findings and Takeways:
+1. Aenean faucibus purus nec felis vehicula, vel varius orci tempus. 
+2. Liquam feugiat ipsum non enim efficitur ornare. Nunc id sapien interdum tortor eleifend volutpat sed ut risus.
+3. Maecenas pulvinar nisl lacinia neque pretium aliquam. 
+4. Phasellus faucibus, justo eu vehicula semper, ipsum urna tincidunt magna, quis eleifend sem orci sed arcu.
---
+++ acquire.py
@@ -0,0 +1,187 @@
+import env
+import pandas as pd 
+from os import path 
+import math
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+
+def get_connection(database):
+    '''
+    Database: string; name of database that the url is being created for
+    '''
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database}'
+
+sql_query = '''
+SELECT prop.*, logerror, transactiondate, airconditioningdesc, architecturalstyledesc, buildingclassdesc, heatingorsystemdesc, propertylandusedesc, storydesc, typeconstructiondesc
+FROM properties_2017 as prop 
+INNER JOIN (
+	SELECT id, p.parcelid, logerror, transactiondate
+	FROM predictions_2017 AS p
+	INNER JOIN (
+		SELECT parcelid,  MAX(transactiondate) AS max_date
+		FROM predictions_2017 
+		GROUP BY (parcelid)) AS sub
+			ON p.parcelid = sub.parcelid
+		WHERE p.transactiondate = sub.max_date
+		) AS subq
+	ON prop.parcelid = subq.parcelid
+LEFT JOIN airconditioningtype
+	ON prop.airconditioningtypeid = airconditioningtype.airconditioningtypeid
+LEFT JOIN architecturalstyletype
+	ON prop.architecturalstyletypeid = architecturalstyletype.architecturalstyletypeid
+LEFT JOIN buildingclasstype 
+	ON prop.buildingclasstypeid = buildingclasstype.buildingclasstypeid
+LEFT JOIN heatingorsystemtype
+	ON prop.heatingorsystemtypeid = heatingorsystemtype.heatingorsystemtypeid
+LEFT JOIN propertylandusetype
+	ON prop.propertylandusetypeid = propertylandusetype.propertylandusetypeid
+LEFT JOIN storytype
+	ON prop.storytypeid = storytype.storytypeid
+LEFT JOIN typeconstructiontype
+	ON prop.typeconstructiontypeid = typeconstructiontype.typeconstructiontypeid
+WHERE latitude IS NOT NULL 
+	AND longitude IS NOT NULL
+	AND transactiondate BETWEEN '2017-01-01' AND '2017-12-31';
+'''
+
+def acquire_cache_zillow():
+    if not path.isfile('zillow.csv'):
+        url = get_connection('zillow')
+        zillow = pd.read_sql(sql_query, url)
+        zillow.to_csv('zillow.csv')
+    zillow = pd.read_csv('zillow.csv')
+    zillow.drop(columns=['Unnamed: 0'], inplace=True)
+    return zillow
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+def map_county(row):
+    if row['fips'] == 6037:
+        return 'Los Angeles'
+    elif row['fips'] == 6059:
+        return 'Orange'
+    elif row['fips'] == 6111:
+        return 'Ventura'
+
+def wrangle_zillow(df):
+	df = df[(df.propertylandusedesc == 'Single Family Residential')|(df.propertylandusedesc == 'Condominium')|(df.propertylandusedesc == 'Planned Unit Development')|(df.propertylandusedesc == 'Mobile Home')|(df.propertylandusedesc == 'Manufactured, Modular, Prefabricated Homes')|(df.propertylandusedesc == 'Residential General')]
+	df = df[df.bedroomcnt > 0]
+	df = df[~(df.unitcnt > 1)]
+	handle_missing_values(df)
+	df.drop(columns=['assessmentyear', 'calculatedbathnbr', 'finishedsquarefeet12', 'propertyzoningdesc', 'regionidcity', 'roomcnt', 'unitcnt'], inplace=True)
+	df = df.dropna(subset=['censustractandblock'])
+	df = df.dropna(subset=['regionidzip'])
+	columns_to_change = ['id', 'parcelid', 'fips', 'heatingorsystemtypeid', 'propertylandusetypeid', 'rawcensustractandblock', 'regionidcounty', 'regionidzip', 'censustractandblock']
+	for column in columns_to_change:
+		df[column] = df[column].astype(object)
+	df['fips'] = df.apply(lambda row: map_county(row), axis = 1)
+	return df
+
+def split_impute_zillow(df, pct=0.10):
+	'''
+	This divides the zillow dataframe into train, validate, and test sets.
+	It then imputes missing values based on previously established best practice.
+	
+	Parameters - (df, pct=0.10)
+    df = dataframe you wish to split
+    pct = size of the test set, 1/2 of size of the validate set
+
+    Returns three dataframes (train, validate, test)
+	'''
+	train_validate, test = train_test_split(df, test_size=pct, random_state = 123)
+	train, validate = train_test_split(train_validate, test_size=pct*2, random_state = 123)
+
+	train['buildingqualitytypeid'].fillna(value=train.buildingqualitytypeid.mean(), inplace=True)
+	validate['buildingqualitytypeid'].fillna(value=train.buildingqualitytypeid.mean(), inplace=True)
+	test['buildingqualitytypeid'].fillna(value=train.buildingqualitytypeid.mean(), inplace=True)
+
+	train['calculatedfinishedsquarefeet'].fillna(value=train.calculatedfinishedsquarefeet.median(), inplace=True)
+	validate['calculatedfinishedsquarefeet'].fillna(value=train.calculatedfinishedsquarefeet.median(), inplace=True)
+	test['calculatedfinishedsquarefeet'].fillna(value=train.calculatedfinishedsquarefeet.median(), inplace=True)
+
+	train['fullbathcnt'].fillna(value=train.fullbathcnt.median(), inplace=True)
+	validate['fullbathcnt'].fillna(value=train.fullbathcnt.median(), inplace=True)
+	test['fullbathcnt'].fillna(value=train.fullbathcnt.median(), inplace=True)
+
+	train['heatingorsystemtypeid'].fillna(value=train.heatingorsystemtypeid.value_counts().index[0], inplace=True)
+	validate['heatingorsystemtypeid'].fillna(value=train.heatingorsystemtypeid.value_counts().index[0], inplace=True)
+	test['heatingorsystemtypeid'].fillna(value=train.heatingorsystemtypeid.value_counts().index[0], inplace=True)	
+
+	train['heatingorsystemdesc'].fillna(value=train.heatingorsystemdesc.value_counts().index[0], inplace=True)
+	validate['heatingorsystemdesc'].fillna(value=train.heatingorsystemdesc.value_counts().index[0], inplace=True)
+	test['heatingorsystemdesc'].fillna(value=train.heatingorsystemdesc.value_counts().index[0], inplace=True)	
+
+	train['lotsizesquarefeet'].fillna(value=train.lotsizesquarefeet.median(), inplace=True)
+	validate['lotsizesquarefeet'].fillna(value=train.lotsizesquarefeet.median(), inplace=True)
+	test['lotsizesquarefeet'].fillna(value=train.lotsizesquarefeet.median(), inplace=True)	
+
+	train['yearbuilt'].fillna(value=train.yearbuilt.mean(), inplace=True)
+	validate['yearbuilt'].fillna(value=train.yearbuilt.mean(), inplace=True)
+	test['yearbuilt'].fillna(value=train.yearbuilt.mean(), inplace=True)	
+
+	train['structuretaxvaluedollarcnt'].fillna(value=train.structuretaxvaluedollarcnt.median(), inplace=True)
+	validate['structuretaxvaluedollarcnt'].fillna(value=train.structuretaxvaluedollarcnt.median(), inplace=True)
+	test['structuretaxvaluedollarcnt'].fillna(value=train.structuretaxvaluedollarcnt.median(), inplace=True)	
+
+	train['taxvaluedollarcnt'].fillna(value=train.taxvaluedollarcnt.median(), inplace=True)
+	validate['taxvaluedollarcnt'].fillna(value=train.taxvaluedollarcnt.median(), inplace=True)
+	test['taxvaluedollarcnt'].fillna(value=train.taxvaluedollarcnt.median(), inplace=True)
+	
+	train['landtaxvaluedollarcnt'].fillna(value=train.landtaxvaluedollarcnt.median(), inplace=True)
+	validate['landtaxvaluedollarcnt'].fillna(value=train.landtaxvaluedollarcnt.median(), inplace=True)
+	test['landtaxvaluedollarcnt'].fillna(value=train.landtaxvaluedollarcnt.median(), inplace=True)
+
+	train['taxamount'].fillna(value=train.taxamount.median(), inplace=True)
+	validate['taxamount'].fillna(value=train.taxamount.median(), inplace=True)
+	test['taxamount'].fillna(value=train.taxamount.median(), inplace=True)
+	
+	return train, validate, test
+
+def prepare_zillow():
+	df = acquire_cache_zillow()
+	df = wrangle_zillow(df)
+	train, validate, test = split_impute_zillow(df)
+	return train, validate, test
+
+def standard_scaler(train, validate, test):
+    '''
+    Accepts three dataframes and applies a standard scaler to convert values in each dataframe
+    based on the mean and standard deviation of each dataframe respectfully. 
+    Columns containing object data types are dropped, as strings cannot be directly scaled.
+
+    Parameters (train, validate, test) = three dataframes being scaled
+    
+    Returns (scaler, train_scaled, validate_scaled, test_scaled)
+    '''
+    # Remove columns with object data types from each dataframe
+    train = train.select_dtypes(exclude=['object'])
+    validate = validate.select_dtypes(exclude=['object'])
+    test = test.select_dtypes(exclude=['object'])
+    # Fit the scaler to the train dataframe
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
+    # Transform the scaler onto the train, validate, and test dataframes
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    validate_scaled = pd.DataFrame(scaler.transform(validate), columns=validate.columns.values).set_index([validate.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, validate_scaled, test_scaled
+
+def scale_inverse(scaler, train_scaled, validate_scaled, test_scaled):
+    '''
+    Takes in three dataframes and reverts them back to their unscaled values
+
+    Parameters (scaler, train_scaled, validate_scaled, test_scaled)
+    scaler = the scaler you with to use to transform scaled values to unscaled values with. Presumably the scaler used to transform the values originally. 
+    train_scaled, validate_scaled, test_scaled = the dataframes you wish to revert to unscaled values
+
+    Returns train_unscaled, validated_unscaled, test_unscaled
+    '''
+    train_unscaled = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train_scaled.index.values])
+    validate_unscaled = pd.DataFrame(scaler.inverse_transform(validate_scaled), columns=validate_scaled.columns.values).set_index([validate_scaled.index.values])
+    test_unscaled = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test_scaled.index.values])
+    return train_unscaled, validate_unscaled, test_unscaled
